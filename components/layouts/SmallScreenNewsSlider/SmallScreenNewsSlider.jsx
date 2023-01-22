b5ca42ba66cb56fe586d5ff6f841f5af5fbccb40
agostinhocfp <69576320+agostinhocfp@users.noreply.github.com>
2023-01-22 08:34:34 +0100

    Top: minor

--- components/layouts/SmallScreenNewsSlider/SmallScreenNewsSlider.jsx
+++ components/layouts/SmallScreenNewsSlider/SmallScreenNewsSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./SmallScreenNewsSlider.module.css";
 import Carousel from "../../molecules/carousel/Carousel";
